9273a1c4038ba9996f8b7eb4d6e6dc67ea8f0aeb
Michael Pories <pories@hotmail.com>
2018-03-11 21:31:00 -0400

    Leet Roman work

---
+++ LeetGrind/LeetGrind/Easy/Roman.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections; //added for hashtable.
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetGrind.Easy
+{
+    public static class Roman
+    {
+
+        public static int RomanToInt(string s)
+
+        {
+
+            if (s.Length == 0)
+                return 0;
+
+            Hashtable ht = new Hashtable();
+
+            ht.Add('I', 1);
+            ht.Add('V', 5);
+            ht.Add('X', 10);
+            ht.Add('L', 50);
+            ht.Add('C', 100);
+            ht.Add('D', 500);
+            ht.Add('M', 1000);
+
+            int i = 0;
+            int number = 0;
+            while (i < s.Length)
+            {
+                if (i + 1 < s.Length && (int)ht[char.ToUpper(s[i])] < (int)ht[char.ToUpper(s[i + 1])])
+                {
+                    number += (int)ht[char.ToUpper(s[i + 1])] - (int)ht[char.ToUpper(s[i])];
+                    i += 2;
+                }
+
+                else
+                {
+                    number += (int)ht[char.ToUpper(s[i])];
+                    i++;
+                }
+            }
+            return number;
+        }
+    }
+}
---
+++ LeetGrind/LeetGrind/Program.cs
@@ -0,0 +1,20 @@
+﻿using LeetGrind.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetGrind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paul = Roman.RomanToInt("xxv");
+            Console.WriteLine(paul);
+            Console.ReadLine();
+
+        }
+    }
+}
